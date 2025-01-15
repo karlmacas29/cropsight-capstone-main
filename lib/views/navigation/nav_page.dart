0870c9cdd72a04a7104c0d1f7edea03a669a25f2
karlracomamacas@gmail.com <karlracomamacas@gmail.com>
2025-01-16 07:38:08 +0800

    modified:   lib/views/navigation/nav_page.dart

--- lib/views/navigation/nav_page.dart
+++ lib/views/navigation/nav_page.dart
@@ -1,13 +1,13 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:cropsight/views/navigation/cropsight.dart';
-// import 'package:cropsight/views/navigation/notifier/change_notifier.dart';
+import 'package:cropsight/views/navigation/notifier/change_notifier.dart';
 import 'package:cropsight/views/navigation/reports_tagging.dart';
 import 'package:cropsight/views/navigation/home.dart';
 import 'package:cropsight/views/navigation/history.dart';
 import 'package:cropsight/views/pages/settings.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePageNav extends StatefulWidget {
@@ -20,7 +20,6 @@ class HomePageNav extends StatefulWidget {
 class _HomePageNavState extends State<HomePageNav> {
   int _currentIndex = 0;
   late List<Widget> tabsnav;
-  String? selectedLocation;
 
   final _iconappbar = [
     FluentIcons.home_12_regular,
@@ -99,7 +98,7 @@ class _HomePageNavState extends State<HomePageNav> {
 
   @override
   Widget build(BuildContext context) {
-    // final locationProvider = Provider.of<LocationProvider>(context);
+    final locationProvider = Provider.of<LocationProvider>(context);
     return Scaffold(
       backgroundColor: Theme.of(context).brightness == Brightness.light
           ? const Color.fromRGBO(244, 253, 255, 1)
@@ -117,47 +116,42 @@ class _HomePageNavState extends State<HomePageNav> {
               scrolledUnderElevation: 0.0,
               actions: [
                 isHide()
-                    ? Container(
-                        child: Row(
-                          children: [
-                            Icon(FluentIcons.location_12_filled),
-                            Text('${selectedValue.toString()}, Panabo'),
-                          ],
+                    ? DropdownButton<String>(
+                        icon: SizedBox.shrink(),
+                        underline: SizedBox.shrink(),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 12,
                         ),
+                        dropdownColor:
+                            Theme.of(context).brightness == Brightness.light
+                                ? const Color.fromRGBO(244, 253, 255, 1)
+                                : const Color.fromRGBO(18, 18, 18, 1),
+                        hint: const Text('Location?'),
+                        value:
+                            selectedValue ?? locationProvider.selectedLocation,
+                        items: dropdownItems.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  FluentIcons.location_12_filled,
+                                  color: Colors.green,
+                                ),
+                                Text(
+                                  "$value , Panabo",
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: null,
                       )
                     : SizedBox.shrink(),
-                // DropdownButton<String>(
-                //     iconEnabledColor: Colors.green,
-                //     icon: const Icon(FluentIcons.location_12_filled),
-                //     dropdownColor:
-                //         Theme.of(context).brightness == Brightness.light
-                //             ? const Color.fromRGBO(244, 253, 255, 1)
-                //             : const Color.fromRGBO(18, 18, 18, 1),
-                //     hint: const Text('Location?'),
-                //     value:
-                //         selectedValue ?? locationProvider.selectedLocation,
-                //     items: dropdownItems.map((String value) {
-                //       return DropdownMenuItem<String>(
-                //         value: value,
-                //         child: Text(value),
-                //       );
-                //     }).toList(),
-                //     onChanged: (String? newValue) {
-                //       locationProvider.updateLocation(newValue);
-                //       setState(() {
-                //         selectedValue = newValue;
-                //       });
-                //       // Save the new value
-                //       _saveValue(newValue);
-                //       Provider.of<LocationProvider>(context, listen: false)
-                //           .updateLocation(newValue);
-
-                //       // Print the current selected value
-                //       print('Selected value: $selectedValue');
-
-                //       _loadSavedValue();
-                //     },
-                //   ),
                 IconButton(
                   iconSize: 30,
                   onPressed: () {
